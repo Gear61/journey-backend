482485dbac10376ce59422b6defc181541fd999e
Gear61 <chessnone@yahoo.com>
2018-03-02 00:18:44 -0800

    Google login

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ var FB = require('fb');
 // PostgreSQL client
 const { Client } = require('pg')
 
+// Oauth client for Google login
+const {OAuth2Client} = require('google-auth-library');
+const GOOGLE_CLIENT_ID = "956612316816-n23cs49obd4fmn1qgs4abhqs7t3f6fnd.apps.googleusercontent.com";
+const google_oauth_client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
 // POST body parsing
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -44,20 +49,40 @@ app.post('/onboarding/facebook', function(request, response) {
 
 	FB.setAccessToken(access_token);
     FB.api('me', { fields: ['name', 'email', 'picture.type(large)'] }, function (res) {
-	  if(!res || res.error) {
-	    response.status(400);
-	    response.send({'error': res.error});
-	    return;
-	  }
+	  	if(!res || res.error) {
+	    	response.status(400);
+		    response.send({'error': res.error});
+			return;
+		}
 
-	  response.status(200);
-	  var user_info = {};
-	  user_info['name'] = res.name;
-	  user_info['email'] = res.email;
-	  user_info['profile_picture_url'] = res.picture.data.url;
-	  response.send({'user_info': user_info});
+		response.status(200);
+		var user_info = {};
+		user_info['name'] = res.name;
+		user_info['email'] = res.email;
+	 	user_info['profile_picture_url'] = res.picture.data.url;
+		response.send(user_info);
 	});
 });
+
+app.post('/onboarding/google', function(request, response) {
+	var id_token = request.body.id_token;
+	verifyGoogleToken(id_token, response).catch(console.error);
+});
+
+async function verifyGoogleToken(token, response) {
+  	const ticket = await google_oauth_client.verifyIdToken({
+		idToken: token,
+		audience: GOOGLE_CLIENT_ID,
+	});
+	const payload = ticket.getPayload();
+
+	response.status(200);
+	var user_info = {};
+	user_info['name'] = payload['name'];
+	user_info['email'] = payload['email'];
+	user_info['profile_picture_url'] = payload['picture'];
+	response.send(user_info);
+}
 
 app.listen(app.get('port'), function() {
   	console.log('Node app is running on port', app.get('port'));
